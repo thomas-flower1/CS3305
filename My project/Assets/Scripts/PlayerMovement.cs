5dc44eaefc19e22c50d3bed033f44cd74e291979
Thomas Flowers <thomas12flower@gmail.com>
2026-03-01 18:49:34 +0000

    WBG model layout done

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float speed = 200f;
+
+    private Vector3 velocity;
+    public float jumpHeight = 3f;
+    public float gravity = -9.81f;
    
     // Update is called once per frame
     void FixedUpdate()
@@ -18,7 +22,18 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
 
+        // Jump
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
         controller.SimpleMove(move * Time.deltaTime * speed);
+
+
         
     }
 
